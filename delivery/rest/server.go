1c694e128d53eec0e19e4f1db995f662d6ae32f8
Arief Hidayatulloh <ariefhidayatulloh@gmail.com>
2020-06-24 15:41:08 +0700

    cors

--- delivery/rest/server.go
+++ delivery/rest/server.go
@@ -6,15 +6,17 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func Serve() {
 	r := mux.NewRouter()
 	r.HandleFunc("/request", RequestHandler).Methods("GET", "POST", "PUT", "DELETE", "SET")
 
+	handler := cors.Default().Handler(r)
 	s := &http.Server{
 		Addr:           ":8185",
-		Handler:        r,
+		Handler:        handler,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
